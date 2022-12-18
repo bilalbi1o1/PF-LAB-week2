039667bfd2095c0c693279127a56f852c92d5df7
Bilal Ahmad <120562994+bilalbi1o1@users.noreply.github.com>
2022-12-19 00:31:35 +0500

    Add files via upload

---
+++ colorpacman.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color o1");
+cout<<"        .:----:..          "<<endl;
+cout<<"     .------------.        "<<endl;
+cout<<"    .--------------.       "<<endl;
+cout<<"   .-------------:.        "<<endl;
+cout<<"  :----------::.           "<<endl;
+cout<<"  ---------:.              "<<endl;
+cout<<"  .-----------:..          "<<endl;
+cout<<"   .--------------:.       "<<endl;
+cout<<"    .------------:.        "<<endl;
+cout<<"     .:---------:.         "<<endl;
+
+
+
+}
---
+++ cow.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 17");
+cout<<"                                                           "<<endl;
+cout<<"                                                           "<<endl;
+cout<<"  o  ^____^                                                        "<<endl;
+cout<<"     (O O)\\___________________                                       "<<endl;
+cout<<"   o                           )                                 "<<endl;
+cout<<"      (__)\\                    )/\\                                "<<endl;
+cout<<"         (                     )    /\\                              "<<endl;
+cout<<"          ( _______________   |        |                              "<<endl;
+cout<<"          ||              WWW||                                      "<<endl;
+cout<<"          ||                 ||                                      "<<endl;
+cout<<"          ||                 ||                                      "<<endl;
+cout<<"          ||                 ||                                      "<<endl;
+
+}
